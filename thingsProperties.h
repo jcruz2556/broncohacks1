6d6ac53dbcf24f08e9b737695a02ac9f94aea77e
ESElectric <152034435+ESElectric@users.noreply.github.com>
2026-04-25 20:25:38 -0700

    Create thingsProperties.h

---
+++ thingsProperties.h
@@ -0,0 +1,30 @@
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <Arduino_NetworkConfigurator.h>
+#include "configuratorAgents/agents/BLEAgent.h"
+#include "configuratorAgents/agents/SerialAgent.h"
+void onLedChange();
+
+CloudInt a0Value;
+CloudInt a1Value;
+CloudInt a3Value;
+CloudInt a5Value;
+bool led;
+
+KVStore kvStore;
+BLEAgentClass BLEAgent;
+SerialAgentClass SerialAgent;
+WiFiConnectionHandler ArduinoIoTPreferredConnection; 
+NetworkConfiguratorClass NetworkConfigurator(ArduinoIoTPreferredConnection);
+
+void initProperties(){
+  NetworkConfigurator.addAgent(BLEAgent);
+  NetworkConfigurator.addAgent(SerialAgent);
+  NetworkConfigurator.setStorage(kvStore);
+  // For changing the default reset pin uncomment and set your preferred pin. Use DISABLE_PIN for disabling the reset procedure.
+  //NetworkConfigurator.setReconfigurePin(your_pin);
+  ArduinoCloud.setConfigurator(NetworkConfigurator);
+
+  ArduinoCloud.addProperty(led, READWRITE, ON_CHANGE, onLedChange);
+
+}
